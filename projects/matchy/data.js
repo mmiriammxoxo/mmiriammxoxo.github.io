9a3dce9d5074f60f77c8a209839f3d39e2de9f4f
Ubuntu <ubuntu@ip-172-31-85-218.ec2.internal>
2020-01-03 13:47:50 +0000

    matchypseudo underpants

--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -11,10 +11,15 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+// declare object variable
 var animal = {};
+// add species key/value pair 
 animal.species = "rabbit";
+// add name key/value pair
 animal["name"] = "bunny";
+// noises key
 animal.noises = [];
+// print object
 console.log(animal);
 
 
@@ -23,20 +28,30 @@ console.log(animal);
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+// declare empty array variable
 var noises = [];
+// add value to noises-array with bracket notation
 noises[0] = ("rrarrr");
+// add value with push
 noises.push("shhshhh");
+// add value to beginning of array
 noises.unshift("mmemmmemm");
+// add another value to end of array
 noises[noises.length] = ("yarrar");
+// print length of noises array
 console.log(noises.length);
+// print last noises element
 console.log(noises[noises.length-1]);
+// print noises array
 console.log(noises);
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+// assign noises arr to animal noises key
 animal["noises"] =noises;
+// add another value to noises property
 noises.push("yeeeyaah");
 
 /* *******************************************************************
@@ -61,14 +76,21 @@ noises.push("yeeeyaah");
 //////////////////////////////////////////////////////////////////////
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+// declare and assign empty array
 animals =[];
+// add animal object to animals array
 animals.push(animal);
+// print animals array
 console.log(animals);
+// declare duck variable and assign given key /values
 var duck = {species: 'duck', 
            name: 'Jerome', 
            noises: ['quack', 'honk', 'sneeze', 'woosh']};
+// add duck object to animals collection           
 animals.push(duck);
+// print animals collection
 console.log(animals);
+// declare and assign 2 more animals with required key value pairs
 var dog = {species: 'dog',
            name: 'Snoopy',
            noises: ['ggrrrr', 'wooof']};
@@ -76,6 +98,7 @@ var cat = {species: 'cat',
            name: 'Jelly',
            noises: ['rrrrr', 'meeow']
 };
+// add dog/ cat objects to animals array
 animals.push(dog, cat);
 
 
--- projects/underpants/underpants.js
+++ projects/underpants/underpants.js
@@ -20,9 +20,11 @@ var _ = {};
 *   _.identity(5) === 5
 *   _.identity({a: "b"}) === {a: "b"}
 */
+// declare function expression that takes one parameter
 _.identity = function(value) {
+    // return given value
     return value;
-}
+};
 
 /** _.typeOf
 * Arguments:
@@ -43,9 +45,22 @@ _.identity = function(value) {
 * _.typeOf("javascript") -> "string"
 * _.typeOf([1,2,3]) -> "array"
 */
+// declare function that takes one parameter
 _.typeOf = function(value) {
-    
-}
+   // check if given value is an array, then return "array"
+   if (Array.isArray(value) === true) {
+       return "array";
+    // check if value is undefined
+    } else if ( value === undefined) {
+        return "undefined";
+    // check if value is null
+    } else if (value === null) {
+        return "null";
+    // return value data type
+    } else {
+      return typeof value;
+    }
+};
 
 /** _.first
 * Arguments:
@@ -64,7 +79,29 @@ _.typeOf = function(value) {
 *   _.first(["a", "b", "c"], 1) -> "a"
 *   _.first(["a", "b", "c"], 2) -> ["a", "b"]
 */
-
+// declare function expression with two parameters
+_.first = function(array, number) {
+    // check if array is an array and whether number is negative
+    if (Array.isArray(array) === false || number < 0 ) {
+        return [];
+    // check if number parameter was provided and whether its a number datatype
+    } else if (number === undefined || typeof number !== "number") {
+        // if not return first element 
+        return array[0];
+     // if number greater than array length return entire array
+    } else if ( array.length < number) {
+        return array;
+    } else {
+        // declare output array
+        var resultArr = [];
+        // for loop to loop through array and extract given number of elements
+        for (var i = 0; i < number; i++) {
+           // add elements to output array
+           resultArr.push(array[i]);
+          // return output array
+        } return resultArr;
+    } 
+    };
 
 /** _.last
 * Arguments:
@@ -83,7 +120,29 @@ _.typeOf = function(value) {
 *   _.last(["a", "b", "c"], 1) -> "c"
 *   _.last(["a", "b", "c"], 2) -> ["b", "c"]
 */
-
+// declare last function with two parameters
+_.last = function(array, number){
+    // check if array is an array and if number is negative
+    if (Array.isArray(array) === false || number < 0 ) {
+        return [];
+    // check if number is not provided or not number data type
+    } else if (number === undefined || typeof number !== "number") {
+        // if so return -1
+        return array[array.length-1];
+      // if number is greater than array length then return entire array
+    } else if ( array.length < number) {
+        return array;
+    } else {
+        // declare result array
+        var resultArr = [];
+        // for-loop through array elements starting from the back 
+        for (var i = array.length - number; i < array.length ; i++) {
+           // add elements to result array
+           resultArr.push(array[i]);
+          // return result
+        } return resultArr;
+    } 
+    };
 
 /** _.indexOf
 * Arguments:
@@ -100,7 +159,17 @@ _.typeOf = function(value) {
 *   _.indexOf(["a","b","c"], "c") -> 2
 *   _.indexOf(["a","b","c"], "d") -> -1
 */
-
+// declare index function with 2 parameters
+_.indexOf = function(array, value){
+    // for-loop to check each array element for a match with given value
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === value ) {
+            // if theres a match return the value
+            return i;
+        } 
+     // if no match return -1
+    }return -1;
+};
 
 /** _.contains
 * Arguments:
@@ -116,7 +185,12 @@ _.typeOf = function(value) {
 * Examples:
 *   _.contains([1,"two", 3.14], "two") -> true
 */
-
+// declare contains function with two parameters
+_.contains = function(array, value){
+   // use include method and ternary operator to check if array contains the
+   // given value, return true or fale
+   return (array.includes(value)) ? true : false;
+}
 
 /** _.each
 * Arguments:
@@ -133,7 +207,22 @@ _.typeOf = function(value) {
 *   _.each(["a","b","c"], function(e,i,a){ console.log(e)});
 *      -> should log "a" "b" "c" to the console
 */
-
+// declare each function with two parameters
+_.each = function(collection, functn) {
+    //check if array is array
+    if (Array.isArray(collection)) {
+        // for-loop to run provided function on each element
+        for( var i= 0; i < collection.length; i++) {
+            functn(collection[i], i, collection);
+        } 
+      // check if collection is object
+    } else if (typeof collection === "object") {
+        // for-loop to run provided function on each object value
+        for (var key in collection) {
+            functn(collection[key], key, collection);
+        }
+    }
+};
 
 /** _.unique
 * Arguments:
@@ -144,6 +233,18 @@ _.typeOf = function(value) {
 * Examples:
 *   _.unique([1,2,2,4,5,6,5,2]) -> [1,2,4,5,6]
 */
+_.unique = function(inputArray) {
+    // declare output array with first element from input array added
+    var noDoubles = [inputArray[0]];
+    // for loop to loop through array values
+    for (var i = 1; i < inputArray.length; i++) {
+        // use indexOf function to check if current element is already in output array
+        if((_.indexOf(noDoubles, inputArray[i])) === -1) {
+            // add elements to output array
+            noDoubles.push(inputArray[i]);
+        }
+    } return noDoubles;
+};
 
 
 /** _.filter
@@ -161,7 +262,18 @@ _.typeOf = function(value) {
 * Extra Credit:
 *   use _.each in your implementation
 */
-
+_.filter = function(arr, funcn){
+   var outP=[];
+   // use each function with param= arr and param = function
+  _.each(arr, function(e, i, array) {
+      // if extracted element passes the test function add it to outP array
+      if (funcn(e, i, array) === true) {
+          // add elements to output array
+           outP.push(e);
+        } 
+});
+  return outP;
+}; 
 
 /** _.reject
 * Arguments:
@@ -175,8 +287,22 @@ _.typeOf = function(value) {
 * Examples:
 *   _.reject([1,2,3,4,5], function(e){return e%2 === 0}) -> [1,3,5]
 */
-
-
+// declare reject function with two parameters
+_.reject = function(arr, func) {
+    // declare and assign output array
+    var rejArr = [];
+    // call filter function to invoke anonymous function to check each array element
+    _.filter(arr, function(e, i, array) {
+        // if condition to check each element
+        if (func(e, i, array) === false) {
+            // add element to output array
+            rejArr.push(e);
+        }
+    });
+    // return output array
+    return rejArr;
+};    
+    
 /** _.partition
 * Arguments:
 *   1) An array
@@ -193,9 +319,25 @@ _.typeOf = function(value) {
 *   _.partition([1,2,3,4,5], function(element,index,arr){
 *     return element % 2 === 0;
 *   }); -> [[2,4],[1,3,5]]
-}
+* 
 */
-
+// declare partition function
+_.partition = function(array, fun) {
+    //declare empty array with two nested empty arrays to hold outputs
+    var results = [[],[]]; 
+    // call each function to invoke anonymous function
+    _.each(array, function(e, k, arr){
+        // if statement to check each array element
+        if (fun(e, k, arr) === true) {
+            // add elements to the result array
+            results[0].push(e);
+        } else {
+            results[1].push(e);
+        }
+    });
+    // return result array
+    return results;
+};
 
 /** _.map
 * Arguments:
@@ -212,7 +354,19 @@ _.typeOf = function(value) {
 * Examples:
 *   _.map([1,2,3,4], function(e){return e * 2}) -> [2,4,6,8]
 */
-
+// declare map function with two parameters
+_.map = function(collection, fun) {
+    // declare output array
+     var alteredArr = [];
+     // invoke each function to access each element of the collection
+      _.each(collection, function(val, index, collect){
+         // call provided function to execute on each element and add the
+         // output to the output array
+         alteredArr.push(fun(val, index, collect));
+      }); 
+      // return array of altered elements
+      return alteredArr;
+};
 
 /** _.pluck
 * Arguments:
@@ -224,7 +378,18 @@ _.typeOf = function(value) {
 * Examples:
 *   _.pluck([{a: "one"}, {a: "two"}], "a") -> ["one", "two"]
 */
-
+// declare map function with two parameters
+_.pluck = function(objArr, prop) {
+    // declare output variable
+    var values =[];
+    // call map function to access each key value
+    _.map(objArr, function(val, index, collec){
+      //add values to output array
+      values.push(val[prop]);  
+    });
+    // return output array
+    return values;
+}; 
 
 /** _.every
 * Arguments:
@@ -246,7 +411,36 @@ _.typeOf = function(value) {
 *   _.every([2,4,6], function(e){return e % 2 === 0}) -> true
 *   _.every([1,2,3], function(e){return e % 2 === 0}) -> false
 */
-
+// declare every function with two parameters
+_.every = function(collection, fun) {
+    // declare array for return results
+    var results=[];
+    // if function parameter was not provided
+    if (fun === undefined ) {
+       // use each function to iterate over each element
+        _.each(collection, function(e, i, coll) {
+            // if boolean value of element is falsey
+            if (e === false) {
+               // add false to result array
+               results.push(false);
+            } 
+        }); 
+        // if result array empty return true
+        return (results.length === 0);
+    }
+     // use each function to iterate over each element 
+     _.each(collection, function(e, i, coll) {
+        // call function on each element and add result to results array
+        results.push(fun(e, i, collection))
+    });
+    // if results array includes false return false
+    if (results.includes(false)){
+        return false;
+    // otherwise return true
+    } else {
+        return true;
+    }
+};
 
 /** _.some
 * Arguments:
@@ -268,7 +462,40 @@ _.typeOf = function(value) {
 *   _.some([1,3,5], function(e){return e % 2 === 0}) -> false
 *   _.some([1,2,3], function(e){return e % 2 === 0}) -> true
 */
-
+_.some = function(collection, fun)  {
+    // declare array for return results
+    var results=[];
+    // if function parameter was not provided
+    if (fun === undefined ) {
+       // use each function to iterate over each element
+        _.each(collection, function(e, i, coll) {
+            // if boolean value of element is falsey
+            if (e === true) {
+               // add true to result array
+               results.push(true);
+            } 
+        }); 
+        // if result array includes true
+        if (results.includes(true)){
+            return true;
+          // otherwise return false
+        } else {
+            return false;
+       }
+    }
+     // use each function to iterate over each element 
+     _.each(collection, function(e, i, coll) {
+        // call function on each element and add result to results array
+        results.push(fun(e, i, collection))
+    });
+    // if results array includes false return false
+    if (results.includes(true)){
+        return true;
+    // otherwise return true
+    } else {
+        return false;
+    }
+};
 
 /** _.reduce
 * Arguments:
@@ -288,7 +515,46 @@ _.typeOf = function(value) {
 * Examples:
 *   _.reduce([1,2,3], function(previousSum, currentValue, currentIndex){ return previousSum + currentValue }, 0) -> 6
 */
+// declare reduce function with 3 parameters
+_.reduce = function(array, fun, seed) {
+    // if seed parameter was not provided
+    if ( seed === undefined) {
+       // seed set to first element of array
+       seed = array[0];
+       // loop through array starting at second element
+       for (var i = 1; i < array.length; i++) {
+           // call function with each element and update seed value to returned value
+           seed = fun(seed, array[i], i);
+       }
+    // return seed
+    return seed;
+    // if seed parameter was provided
+    } else {
+    // call each function to access each element of the array
+    _.each(array, function(e, i , coll) {
+        // call function with each element and update seed value to returned value
+        seed = fun(seed, e, i);
+    });
+}
+ // return seed
+ return seed;   
+}; 
 
+/*
+_.reduce = function(array, fun, seed) {
+    if ( seed === undefined) {
+       seed = array[0];
+       let array1 =array.slice(1);
+        _.each(array1, function(e, i , coll) {
+        seed = fun(seed, e, i+1);
+    });
+    } else {
+    _.each(array, function(e, i , coll) {
+        seed = fun(seed, e, i);
+    });
+}
+ return seed;   
+};  */
 
 /** _.extend
 * Arguments:
@@ -304,6 +570,18 @@ _.typeOf = function(value) {
 *   _.extend(data, {b:"two"}); -> data now equals {a:"one",b:"two"}
 *   _.extend(data, {a:"two"}); -> data now equals {a:"two"}
 */
+// declare extend function with unknown amount of arguments
+_.extend = function(...objects){
+    // declare array of objects from arguments
+    var objsArr = Array.from(arguments);
+    // each function to access each object
+    _.each(objsArr, function(e, i, arr) {
+        // copy values from all objects to first object
+        Object.assign(objsArr[0], e);
+    });
+    //return updated object
+    return objsArr[0];
+} 
 
 //////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE ////////////////////////////////////////////
